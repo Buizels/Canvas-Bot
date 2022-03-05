97391bb3b15c147812e351e453c68334dcb0b43d
ajgrant6 <aj.grant6@icloud.com>
2022-03-05 15:08:13 -0800

    Added get assignments

--- canv.py
+++ canv.py
@@ -7,7 +7,7 @@ from pytz import timezone
 def get_anouncement(course_id):
     """
     This function gets the most recent anouncement of the course.
-    
+
     """
     canvas = Canvas(LINK, API_TOKEN)
     course = canvas.get_course(course_id)
@@ -19,8 +19,8 @@ def get_anouncement(course_id):
 def get_anouncements(course_id):
     """
     This function gets the most recent anouncement of the course.
-    Returns as a string.
-    
+    Returns as a paginated list.
+
     """
     canvas = Canvas(LINK, API_TOKEN)
     course = canvas.get_course(course_id)
@@ -44,13 +44,38 @@ def get_next_assignment(course_id):
     current = datetime.now()
     current = current.astimezone(timezone('US/Pacific'))
 
-    next_assign = assignments[0]
+    next_assignment = assignments[0]
     for i in assignments:
         if(i.due_at != None):
             time = parser.parse(i.due_at)
             time = time.astimezone(timezone('US/Pacific'))
             if(time > current):
-                next_assign = i
+                next_assignment = i
                 break
 
-    return next_assign
+    return next_assignment
+
+def get_next_assignments(course_id):
+    """
+    Returns a list of assignments that are due after the current time.
+    Use <assignment>[i].due_at to get the due date.
+    Use <assignment>[i].name to get the name of the assignment.
+    
+    """
+    canvas = Canvas(LINK, API_TOKEN)
+    course = canvas.get_course(course_id)
+   
+    assignments = course.get_assignments()
+
+    current = datetime.now()
+    current = current.astimezone(timezone('US/Pacific'))
+
+    next_assignments = []
+    for i in assignments:
+        if(i.due_at != None):
+            time = parser.parse(i.due_at)
+            time = time.astimezone(timezone('US/Pacific'))
+            if(time > current):
+                next_assignments += [i]
+
+    return next_assignments
