4a482514fdb9a9cbd744d24086b7940b9c600945
ajgrant6 <aj.grant6@icloud.com>
2022-03-05 13:08:50 -0800

    Update test.py
    comments

--- test.py
+++ test.py
@@ -5,6 +5,9 @@ from canvasapi import Canvas
 course_id = "23263"
 canvas = Canvas(LINK, API_TOKEN)
 
+# Get the course
 next_assign = canv.get_next_assignment(course_id)
+# Get the next assignment
 print(next_assign.name + " is due at: " + str(next_assign.due_at))
+# Print the name of the next assignment and the due date
 print("Latest anouncement: " + str(canv.get_anouncement(course_id)))
