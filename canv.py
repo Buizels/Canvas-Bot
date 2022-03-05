d70ec52ff8f2d192b360c2136f6d9ebd7a285276
ajgrant6 <aj.grant6@icloud.com>
2022-03-05 00:25:16 -0800

    Most recent due date function

--- canv.py
+++ canv.py
@@ -1,6 +1,8 @@
-from secrets import API_TOKEN 
-from secrets import LINK
+from secrets import API_TOKEN, LINK
 from canvasapi import Canvas
+from datetime import datetime
+from dateutil import parser
+from pytz import timezone
 
 def get_anouncement(course_id):
     """
@@ -14,3 +16,26 @@ def get_anouncement(course_id):
 
     return announce[0]
 
+def get_next_assignment(course_id):
+    """
+    This function gets the next assignment of the course.
+    
+    """
+    canvas = Canvas(LINK, API_TOKEN)
+    course = canvas.get_course(course_id)
+   
+    assignments = course.get_assignments()
+
+    current = datetime.now()
+    current = current.astimezone(timezone('US/Pacific'))
+
+    next_assign = assignments[0]
+    for i in assignments:
+        if(i.due_at != None):
+            time = parser.parse(i.due_at)
+            time = time.astimezone(timezone('US/Pacific'))
+            if(time > current):
+                next_assign = i
+                break
+
+    return next_assign
