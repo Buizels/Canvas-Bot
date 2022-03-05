7fed5efaf77e1d94a3ee1e9a1dc3e2fb04a249ae
ajgrant6 <aj.grant6@icloud.com>
2022-03-04 23:06:23 -0800

    first function :)

---
+++ canv.py
@@ -0,0 +1,16 @@
+from secrets import API_TOKEN 
+from secrets import LINK
+from canvasapi import Canvas
+
+def get_anouncement(course_id):
+    """
+    This function gets the most recent anouncement of the course
+    
+    """
+    canvas = Canvas(LINK, API_TOKEN)
+    course = canvas.get_course(course_id)
+   
+    announce = canvas.get_announcements([course_id])
+
+    return announce[0]
+
