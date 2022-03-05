d21269d9c34366e0738323c6cf9790a6d5359902
Emmanuel Jerez <75187415+simba369@users.noreply.github.com>
2022-03-05 15:23:32 -0800

    bot code

--- main.py
+++ main.py
@@ -10,6 +10,18 @@ client = discord.Client()
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
+@client.event()
+async def on_ready():
+    username = str(message.author).split('#')[0]
+    user_message = str(message.contect)
+    channel = str(message.channel.name)
+    print(f'{username}: {user_message} ({channel})')
 
+    if message.author ==  client.user:
+        return
+
+    if message.channel.name == 'testing':
+        if user_message.lower() == 'hello':
+            await message.channe.send(f'fuck you {username}')
 
 client.run(TOKEN)
