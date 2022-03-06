242694427af4e763b0b9e1a25a5f547277d16642
Rvfi <1726rafi@gmail.com>
2022-03-05 18:53:12 -0800

    main.py Commands

--- main.py
+++ main.py
@@ -7,11 +7,11 @@ import canv
 
 client = discord.Client()
 
-@client.event
+@client.event # Bot Status
 async def on_ready():
     print('We have logged in as {0.user}'.format(client))
 
-@client.event
+@client.event 
 async def on_message(message):
     username = str(message.author).split('#')[0]
     user_message = str(message.content)
@@ -26,25 +26,26 @@ async def on_message(message):
         if user_message.lower() == '!anon':
             await message.channel.send(canv.get_anouncement(course_id))
             # await message.channel.send("\n")
-            await message.channel.send(canv.get_anouncement_content(canv.get_anouncement(course_id).message))
+            await message.channel.send(canv.get_anouncement_content(canv.get_anouncement(course_id).message))        
 
             return
+
 
         elif user_message.lower() == '!anons':
             await message.channel.send(canv.get_anouncements(course_id))
 
             return
-
-        # announcement code           
+        
         elif user_message.lower() == "!na": 
             await message.channel.send(canv.get_next_assignment(course_id))
             return
         
         elif user_message.lower() == "!nas":
-            await message.channel.send(canv.get_next_assignments(course_id)(course_id))
+            await message.channel.send(canv.get_anouncements(course_id))
             return
-        
-        
 
+        elif user_message.lower() == "!nas":
+            await message.channel.send(canv.get_anouncements(course_id))
+            return
 
 client.run(TOKEN)
