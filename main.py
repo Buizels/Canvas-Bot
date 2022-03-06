dfbaf74c0104d7a3e6c6bdfb61bcf993bedcc6a3
Rvfi <1726rafi@gmail.com>
2022-03-06 01:25:54 -0800

    Final Final Push, Help Command

--- main.py
+++ main.py
@@ -64,21 +64,15 @@ async def embedMessage(message):
         next_assign = canv.get_next_assignment(course_id)
         await message.channel.send(next_assign.name + " is due at: " + str(next_assign.due_at))        
 
-        returns
+        return
 
 @bot.command(name = 'nas') #get next multiple assignments
 async def embedMessage(message):
     if message.channel.name == 'testing':
-<<<<<<< HEAD
-        next_assignments = canv.get_next_assignments(course_id)
-        for i in next_assignments:
-            await message.channel.send(i.name + " is due at: " + str(i.due_at))      
-=======
         next_assigns = canv.get_next_assignments(course_id)
         string = ''
         for i in  next_assigns:
             string += str(i.name) + " is due at: " + str(canv.convert_to_pst(i.due_at)) + "\n"
->>>>>>> c52f39a86685ff34bd0b991e0229311218eba7ac
 
         await message.channel.send(string)
         return
